9e2b4795fad828611662f22066eedb1c6d602f25
bacho1601 <stevengochkov@gmail.com>
2024-10-16 14:06:20 +0300

    Create VendingMachine.java
    Switch case with ability to check multiple times using a for() loop and somewhat user friendly

---
+++ School Programmes/VendingMachine.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class VendingMachine {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 1; i > 0; i++) {
+            System.out.println("_________\nInput the product code: ");
+            String code = scan.nextLine();
+            switch (code) {
+                case "A1":
+                    System.out.println("Product \"Soda\" - Price: $1.50");
+                    break;
+                case "B2":
+                    System.out.println("Product \"Chips\" - Price: $1.25");
+                    break;
+                case "C3":
+                    System.out.println("Product \"Candy\" - Price: $0.75");
+                    break;
+                case "D4":
+                    System.out.println("Product \"Water\" - Price: $1.00");
+                    break;
+                default:
+                    System.out.println("Error. Please type existing code.");
+            }
+            System.out.println("---------\nTo prompt again, press enter. \n" +
+                                                "To exit the program, type \"end\".");
+            String a = scan.nextLine();
+            if (a.equals("end")){return;} else {System.out.println("Please press enter.");}
+        }
+    }
+}
