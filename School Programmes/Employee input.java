b73f7efbb35175055cdeec8dc0c651547613b23c
bacho1601 <stevengochkov@gmail.com>
2024-10-23 23:20:30 +0300

    Update Employee input.java
    "fixed" error msgs
    //I wanna sleep
    //I have a personal statement for tomorrow cus shes gonna check it, we have an appointment, meow

--- School Programmes/Employee input.java
+++ School Programmes/Employee input.java
@@ -4,43 +4,42 @@ import java.util.Scanner;
 public class EmployeeInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Scanner reader = new Scanner(System.in);
 
         Employee employee = new Employee("John Dee", 101, 5000);
         System.out.println("Employee 1 Details:");
         employee.displayDetails("John Dee", 101, 5000);
 
         for (int i = 2; i >= 2; i++) {
+            System.out.println("!Type in \"end.program\" wherever to end the program.");
             System.out.println("Enter details for a new employee:");
-            try {
-                System.out.print("Name: ");
-                String name = scanner.nextLine();
-                //check if user wants to end program
-                Employee.endProgramCheck(name);
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+            //check if user wants to end program
+            Employee.endProgramCheck(name);
 
-                System.out.print("Employee ID: ");
-                int employeeId = reader.nextInt();
-                System.out.print("Monthly Salary: ");
-                double salary = reader.nextDouble();
+            System.out.print("Employee ID: ");
+            String intemployeeId = scanner.nextLine();
+            Employee.endProgramCheck(intemployeeId);
+            int employeeId = Integer.parseInt(intemployeeId);
 
-                employee = new Employee(name, employeeId, salary);
-                System.out.println("Employee " + i + " details:");
-                employee.displayDetails(name, employeeId, salary);
+            System.out.print("Monthly Salary: ");
+            String doublesalary = scanner.nextLine();
+            Employee.endProgramCheck(doublesalary);
+            double salary = Double.parseDouble(doublesalary);
+            /* parsing as int is the only option
+            i found for the error catch checks to work. Otherwise, using try {} catch () {},
+            e.getMessage() = null (and subsequently I get a console error message, not mine) */
+            System.out.println("");
 
-            } //check for invalid input and "end" instances in any field, just in case
-            catch (InputMismatchException e) {
-                if(e.getMessage().equalsIgnoreCase("end")) {
-                    System.out.println("Shutting down program...");
-                    break;
-                } else {
-                    System.out.println("Invalid input");
-                    scanner.next();
-                    reader.next();
-                }
-            }
+            employee = new Employee(name, employeeId, salary);
+            System.out.println("Employee " + i + " details:");
+            employee.displayDetails(name, employeeId, salary);
+
+            //check for invalid input and "end" instances in any field, just in case
         }
         scanner.close();
-        reader.close();
+        // saw online that this helps prevent data leaks,
+        // I'll try to implement it in my practices from now on
     }
 }
 
@@ -67,8 +66,9 @@ class Employee {
     }
 
     static void endProgramCheck(String string) {
-        if (string.equalsIgnoreCase("end")) {
+        if (string.equalsIgnoreCase("end.program")) {
             System.out.println("Shutting down program...");
-            System.exit(0);        }
+            System.exit(0);
+        }
     }
 }
