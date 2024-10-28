b25f13e163ea018497d0282ccb19aead372afb15
bacho1601 <stevengochkov@gmail.com>
2024-10-28 10:07:55 +0200

    Update firstGUI.java

--- School Programmes/firstGUI.java
+++ School Programmes/firstGUI.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.lang.*;
+
+import static java.lang.String.valueOf;
 
 public class firstGUI{
     public static void main(String[] args) {
@@ -13,35 +16,51 @@ public class firstGUI{
         JButton multiplyFuncButton = new JButton("*");
         JButton addFuncButton = new JButton("+");
         JButton subtractFuncButton = new JButton("-");
-        JLabel inputLabel = new JLabel();
-        JLabel outputLabel = new JLabel();
+        JButton resetButton = new JButton("Reset");
+        JButton equalsButton = new JButton("=");
+        JTextField displayNum1 = new JTextField();
+        JTextField displayNum2 = new JTextField();
+        JTextField displayFunc = new JTextField();
+        JTextField displayResult = new JTextField();
 
         // place and size for components
         // setBounds(x position, y position, width, height)
-        inputLabel.setBounds(300,100,400,50);
-        inputLabel.setFont(new Font("Impact", Font.BOLD,32));
-        inputLabel.setForeground(Color.MAGENTA);
+        displayNum1.setBounds(50,100,100,50);
+        displayNum1.setFont(new Font("Impact", Font.BOLD,20));
+        displayNum1.setForeground(Color.MAGENTA);
 
-        outputLabel.setBounds(100,100,400,50);
-        outputLabel.setFont(new Font("Impact", Font.ITALIC, 32));
-        outputLabel.setForeground(Color.BLUE);
+        displayNum2.setBounds(180,100,100,50);
+        displayNum2.setFont(new Font("Impact", Font.ITALIC, 20));
+        displayNum2.setForeground(Color.BLUE);
+
+        displayFunc.setBounds(150, 100, 30, 50);
+        displayFunc.setFont(new Font("Roboto", Font.BOLD, 25));
+        displayFunc.setForeground(Color.CYAN);
+
+        displayResult.setBounds(170, 200, 300, 50);
+        displayResult.setFont(new Font("Roboto", Font.BOLD, 25));
+        displayResult.setForeground(Color.BLACK);
 
         divideFuncButton.setBounds(350, 300, 100, 50);
         multiplyFuncButton.setBounds(150,300,100,50);
         addFuncButton.setBounds(250, 300, 100, 50);
         subtractFuncButton.setBounds(50, 300, 100, 50);
+        equalsButton.setBounds(350, 100, 100, 50);
+        resetButton.setBounds(100, 100, 100, 50);
 
         // add components to JFrame f
         frame.add(multiplyFuncButton);
         frame.add(divideFuncButton);
         frame.add(addFuncButton);
         frame.add(subtractFuncButton);
-        frame.add(inputLabel);
-        frame.add(outputLabel);
+        frame.add(displayNum1);
+        frame.add(displayNum2);
+        frame.add(displayFunc);
+        frame.add(displayResult);
 
         // make the frame visible
         frame.setVisible(true);
-        outputLabel.setVisible(true);
-        inputLabel.setVisible(true);
+        displayNum2.setVisible(true);
+        displayNum1.setVisible(true);
     }
 }
