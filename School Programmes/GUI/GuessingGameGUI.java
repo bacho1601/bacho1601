4ea19c3c13a8937be3d43c33171f9bf92be81b4c
bacho1601 <stevengochkov@gmail.com>
2025-01-17 13:38:12 +0200

    Update GuessingGameGUI.java
    made it functional (added attempts (can't terminate game when too many attempts), input GUI, have to format it (just make elements bigger, center the output box, move everything down slightly(?), make pretty colours))

--- School Programmes/GUI/GuessingGameGUI.java
+++ School Programmes/GUI/GuessingGameGUI.java
@@ -3,33 +3,61 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.util.Random;
+import java.util.concurrent.Flow;
 
 public class GuessingGameGUI{
     public static void main(String[] args){
-        JFrame window = new JFrame("Guessing game");
-        window.setSize(500,500);
-        window.setLayout(new GridLayout(3, 1));
-
-        JPanel panel4all = new JPanel(new FlowLayout());
-        JLabel returnbox = new JLabel();
-        JTextField inputbox = new JTextField(10);
-        JButton checkerinit = new JButton("Guess");
-
-        panel4all.add(returnbox);
-        panel4all.add(inputbox);
-        panel4all.add(checkerinit);
 
         int numberrand = randomnumber();
+        final int[] attempts = {10};
+
+        JFrame window = new JFrame("Guessing game");
+        window.setSize(500,500);
+        window.setLayout(new GridLayout(4, 1));
+
+        JPanel paneloutput = new JPanel();
+        paneloutput.setLayout(null);
+        JLabel returnbox = new JLabel();
+        //JLabel.se
+        paneloutput.add(returnbox, BorderLayout.SOUTH);
+
+        JPanel panelcheck = new JPanel(new FlowLayout());
+        JButton checkerinit = new JButton("Guess");
+        panelcheck.add(checkerinit);
+
+        JPanel panelinput = new JPanel(new FlowLayout());
+        JTextField inputbox = new JTextField(10);
+        JLabel attemptsshow = new JLabel("You have " + " attemps left");
+        panelinput.add(inputbox);
+        panelinput.add(attemptsshow);
+
+        window.add(paneloutput);
+        window.add(panelcheck);
+        window.add(panelinput);
 
         checkerinit.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e) {
-                // impelement addition
-                calculate('+', textBox1, textBox2, result);
+                int guessednum = Integer.parseInt(inputbox.getText());
+                if(attempts[0] > 0) {
+                    try {
+                        if(guessednum==numberrand) {
+                            returnbox.setText("Correct! You Guessed The Number!");
+                        } else if (guessednum<numberrand){
+                            returnbox.setText("Too low. Try again!");
+                            attempts[0]--;
+                            attemptsshow.setText("You have " + attempts[0] + " attemps left");
+                        } else {
+                            returnbox.setText("Too high. Try again!");
+                            attempts[0]--;
+                            attemptsshow.setText("You have " + attempts[0] +" attemps left");
+                        }
+                    } catch (NumberFormatException ee) {
+                        returnbox.setText("Please enter a valid number.");
+                    }
+                }
+
             }
         });
-
-
-
 
         window.setVisible(true);
 
@@ -39,11 +67,4 @@ public class GuessingGameGUI{
         int numberrand = rand.nextInt(99)+1;
         return numberrand;
     }
-    public static void check(int guessednum, int numberrand){
-        if (guessednum == numberrand) {
-            
-        }
-    }
-
-
 }
