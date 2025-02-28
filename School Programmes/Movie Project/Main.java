a0455fe0aefdaf8e99bc27638f5cc09b2101b46c
bacho1601 <stevengochkov@gmail.com>
2025-02-28 13:34:42 +0200

    Update Main.java
    done literally nothing

--- School Programmes/Movie Project/Main.java
+++ School Programmes/Movie Project/Main.java
@@ -1,2 +1,32 @@
-kur
-  
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    public static void main(String[] args) {
+        Movies[] movies;
+
+        JFrame frame = new JFrame();
+        frame.setSize(700, 500);
+        frame.setVisible(true);
+        frame.setLayout(new BorderLayout());
+
+        JPanel MovieAdd = new JPanel();
+        MovieAdd.setLayout(new FlowLayout());
+
+        
+
+    }
+}
+
+class Movies{
+    String title;
+    String genre;
+    Movies(String title, String genre){
+        this.title = title;
+        this.genre = genre;
+    }
+
+
+}
