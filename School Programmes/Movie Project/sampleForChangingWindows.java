f35b40c306b807b7702790a34c18b518056cec1f
bacho1601 <stevengochkov@gmail.com>
2025-03-05 13:21:43 +0200

    Update sampleForChangingWindows.java

--- School Programmes/Movie Project/sampleForChangingWindows.java
+++ School Programmes/Movie Project/sampleForChangingWindows.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class ChangingWindowsExample {
+public class sampleForChangingWindowsExample {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new ChangingWindowsExample().createGUI());
