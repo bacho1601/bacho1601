db0d9aa020a9e585d301f01fcf4171469184a589
bacho1601 <stevengochkov@gmail.com>
2025-03-05 13:35:05 +0200

    Update Main.java
    hardcore unfinished

--- School Programmes/Movie Project/Main.java
+++ School Programmes/Movie Project/Main.java
@@ -12,10 +12,38 @@ public class Main {
         frame.setVisible(true);
         frame.setLayout(new BorderLayout());
 
+        JPanel cardPanel = new JPanel();
+        CardLayout cardLayout = new CardLayout();
+        cardPanel.setLayout(cardLayout);
+
+        JPanel HomePanel = new JPanel();
+        HomePanel.setLayout(new FlowLayout());
+
+
         JPanel MovieAdd = new JPanel();
         MovieAdd.setLayout(new FlowLayout());
+        JButton homeButtMovie = new JButton("Home");
 
-        
+
+
+        // Add panels to the cardPanel (CardLayout container)
+        cardPanel.add(panel1, "Panel 1");
+        cardPanel.add(panel2, "Panel 2");
+
+        // Add action listeners to buttons to switch between panels
+        homeButtMovie.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cardLayout.show(cardPanel, "Panel 2");
+            }
+        });
+
+        nextButton2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cardLayout.show(cardPanel, "Panel 1");
+            }
+        });
 
     }
 }
