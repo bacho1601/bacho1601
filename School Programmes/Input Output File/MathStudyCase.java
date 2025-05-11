fe8e7cb3516ec2c3d54d3d8933884b77a3aa2dcf
bacho1601 <stevengochkov@gmail.com>
2025-05-11 16:42:33 +0300

    Update MathStudyCase.java
    fixed, works, bufferedwriter appends, / and * functionality removed

--- School Programmes/Input Output File/MathStudyCase.java
+++ School Programmes/Input Output File/MathStudyCase.java
@@ -1,8 +1,6 @@
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.util.Scanner;
 import java.util.Random;
 
@@ -10,12 +8,13 @@ public class MathStudyCase{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
+        Random rand1 = new Random();
         boolean correct = false;
         for (int i = 0; i < 5; i++) {
             correct = false;
-            int int1 = (int) Math.random();
-            int int2 = (int) Math.random();
-            int pick = rand.nextInt(4)+1;
+            int int1 = rand.nextInt(100);
+            int int2 = rand1.nextInt(100);
+            int pick = rand.nextInt(2)+1;
             int result = 0;
             char operat = ' ';
             switch (pick) {
@@ -27,29 +26,23 @@ public class MathStudyCase{
                     result = int1 - int2;
                     operat = '-';
                     break;
-                case 3:
-                    result = int1 / int2;
-                    operat = '/';
-                    break;
-                case 4:
-                    result = int1 * int2;
-                    operat = '*';
-                    break;
             }
-            System.out.print(pick);
-            System.out.println(int1 + operat + int2 + " = ?");
+            System.out.println(int1 + String.valueOf(operat) + int2 + " = ?");
             int guess = scanner.nextInt();
-            FileWrite(int1 + operat + int2 + " = ?\n " + guess + "\n");
+            //System.out.println(guess);
+            //System.out.println(result);
             if(result == guess){
                 correct = true;
             }
+            FileWrite(int1 + operat + int2 + " = ?\n " + guess + "\n");
+            System.out.println(correct);
             FileWrite(String.valueOf(correct) + "\n");
         }
 
     }
     public static void FileWrite(String input) {
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("/home/sgochkov26/Desktop/sample.txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("/home/sgochkov26/Desktop/sample.txt", true));
             writer.write(input);
             writer.close();
         } catch (IOException e) {
